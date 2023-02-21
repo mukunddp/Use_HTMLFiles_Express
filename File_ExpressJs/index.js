5bedb7b3164d9df6c6478220690f50f95b25558d
Mukund Parve <98021056+mukunddp@users.noreply.github.com>
2023-02-21 11:03:35 +0530

    Add files via upload

---
+++ File_ExpressJs/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 4000
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello World!' +  req.params.name)
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+  // res.status(500)
+  // res.json({"name": 34})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
